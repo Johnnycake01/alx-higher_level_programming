3a2e3e22992a0d7f8b9a252977f9789a553770a8
Johnson Oyesina <37639025+Johnnycake01@users.noreply.github.com>
2023-01-03 04:28:11 +0100

    more on classes and test driven development

---
+++ 0x07-python-test_driven_development/tests/README.md
@@ -0,0 +1,1 @@
+Test folder for python scripts
---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 27 2022
+@author: Godday Okoduwa
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle shape
+
+    Attributes:
+        empty
+    """
+
+    pass
